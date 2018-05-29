83f0bf5a26ad68c75a6f6872fb8028049bec8e2f
unknown <shilpa.khandelwal2488@gmail.com>
2018-05-29 10:59:47 -0400

    first commit

---
+++ workspace/v1/middleware/index.js
@@ -0,0 +1,99 @@
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+// all the middleare goes here
+var middlewareObj = {};
+var alertRedInput = "#8C1010";
+var defaultInput = "rgba(10, 180, 180, 1)";
+
+function userNameValidation(usernameInput) {
+    var username = document.getElementById("username");
+    var issueArr = [];
+    if (/[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(usernameInput)) {
+        issueArr.push("No special characters!");
+    }
+    if (issueArr.length > 0) {
+        username.setCustomValidity(issueArr);
+        username.style.borderColor = alertRedInput;
+    } else {
+        username.setCustomValidity("");
+        username.style.borderColor = defaultInput;
+    }
+}
+
+function passwordValidation(passwordInput) {
+    var password = document.getElementById("password");
+    var issueArr = [];
+    if (!/^.{7,15}$/.test(passwordInput)) {
+        issueArr.push("Password must be between 7-15 characters.");
+    }
+    if (!/\d/.test(passwordInput)) {
+        issueArr.push("Must contain at least one number.");
+    }
+    if (!/[a-z]/.test(passwordInput)) {
+        issueArr.push("Must contain a lowercase letter.");
+    }
+    if (!/[A-Z]/.test(passwordInput)) {
+        issueArr.push("Must contain an uppercase letter.");
+    }
+    if (issueArr.length > 0) {
+        password.setCustomValidity(issueArr.join("\n"));
+        password.style.borderColor = alertRedInput;
+    } else {
+        password.setCustomValidity("");
+        password.style.borderColor = defaultInput;
+    }
+}
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+           if(err){
+               req.flash("error", "Campground not found");
+               res.redirect("back");
+           }  else {
+               // does user own the campground?
+            if(foundCampground.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+               res.redirect("back");
+           }  else {
+               // does user own the comment?
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
